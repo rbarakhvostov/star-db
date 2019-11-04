70ce3a732bdb53edd130551b13570b5afb0a256d
rbarakhvostov <rbarakhvostov@gmail.com>
2019-11-04 21:39:11 +0300

    implement authorization system

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,18 +4,30 @@ import Header from '../header';
 import RandomPlanet from '../random-planet';
 import SwapiService from '../../services/swapi-service';
 import DummySwapiService from '../../services/dummy-swapi-service';
-import {PeoplePage, PlanetsPage, StarshipsPage } from '../pages';
+import {
+  PeoplePage,
+  PlanetsPage,
+  StarshipsPage,
+  SecretPage,
+  LoginPage } from '../pages';
 import ErrorBoundary from '../error-boundary';
 import { SwapiServiceProvider } from '../swapi-service-context';
 
 import './app.css';
-import { StarshipDetails, PersonDetails } from '../sw-components';
+import { StarshipDetails } from '../sw-components';
 
 export default class App extends Component {
 
   state = {
     showRandomPlanet: true,
     swapiService : new SwapiService(),
+    isLoggedIn: false,
+  }
+
+  onLogin = () => {
+    this.setState({
+      isLoggedIn: true,
+    });
   }
 
   onServiceChange = () => {
@@ -39,9 +51,8 @@ export default class App extends Component {
   }
 
   render () {
-    const { showRandomPlanet } = this.state;
+    const { showRandomPlanet, isLoggedIn } = this.state;
     const randomPlanet = showRandomPlanet ? <RandomPlanet /> : null;
-  
     return (
       <ErrorBoundary>
         <SwapiServiceProvider value={this.state.swapiService}>
@@ -65,6 +76,18 @@ export default class App extends Component {
                        const { id } = match.params;
                        return <StarshipDetails itemId={id} />
                      }} />
+              <Route
+                path='/login'
+                render={() => (
+                  <LoginPage
+                    isLoggedIn={isLoggedIn}
+                    onLogin={this.onLogin} />
+                )} />
+              <Route
+                path='/secret'
+                render={() => (
+                  <SecretPage isLoggedIn={isLoggedIn} />
+                )}/>
             </div>
           </Router>
         </SwapiServiceProvider>
