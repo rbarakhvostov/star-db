370b21e3d728ec9e28bf4587a6d9d684ef93543b
rbarakhvostov <rbarakhvostov@gmail.com>
2019-11-08 02:44:10 +0300

    code refactoring

--- src/components/error-indicator/error-indicator.js
+++ src/components/error-indicator/error-indicator.js
@@ -6,13 +6,13 @@ const ErrorIndicator = () => {
   return (
     <div className='error-indicator'>
       <img src={icon} alt='error-icon'/>
-      <span className='boom'>BOOM!</span>
-      <span>
+      <p className='boom'>BOOM!</p>
+      <p>
         something has gone terribly wrong
-      </span>
-      <span>
+      </p>
+      <p>
         (but we've already sent droids to fix it)
-      </span>
+      </p>
     </div>
   );
 }
--- src/components/record/record.js
+++ src/components/record/record.js
@@ -11,5 +11,4 @@ const Record = ({ item, field, label }) => {
   )
 }
 
-
 export default Record;
